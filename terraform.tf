e5582684f1cdedc059a6edb187c1bec4d880a8c7
adammontlake <92934931+adammontlake@users.noreply.github.com>
2023-01-22 14:16:02 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -15,23 +15,47 @@ locals {
   environment  = "production"
   service_name = "terraform"
   account_tier = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "random_string" "random_suffix" {
+  length   = 4
+  special  = false
+  upper    = false
 }
 
 data "azurerm_resource_group" "rg" {
   name = "IaC_pipelines_rg"
 }
 
-# Module to create a storage account with website
-module "storage_module" {
-  source                  = "git::https://github.com/adammontlake/IaC-TF-pipe-demo.git//IaC/modules/storage"
-  service_name            = local.service_name
-  resource_group_name     = data.azurerm_resource_group.rg.name
-  location                = data.azurerm_resource_group.rg.location
-  account_tier            = local.account_tier
-  environment             = local.environment
-  terraform_state_storage = false
+# Storage account with website (From Itamar)
+resource "azurerm_storage_account" "example-sa" {
+  name                     = "websitestorage${random_string.random_suffix.result}"
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = local.account_tier
+  account_replication_type = local.account_replication_type
+
   tags = {
-    environment = "production"
-    costcenter  = "it"
+    environment = "test"
   }
+}
+
+resource "azurerm_storage_container" "websitecontainer" {
+  name                  = "sitecontainer"
+  storage_account_name  = azurerm_storage_account.example-sa.name
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_blob" "content" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.example-sa.name
+  storage_container_name = azurerm_storage_container.testcontainer.name
+  type                   = "Block"
+  source                 = "index.html"
+  content_type           = "text/html"
+}
+
+output "site_address" {
+  value = azurerm_storage_blob.content.url
 }
