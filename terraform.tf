896377149f48715b5eb274faea34ea5d6f5dd00a
adammontlake <92934931+adammontlake@users.noreply.github.com>
2023-01-22 13:01:56 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -5,12 +5,6 @@ terraform {
       version = "= 3.30.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "rg_tf_launchpad"
-    storage_account_name = "storagetflaunchpad"
-    container_name       = "tfstate"
-    key                  = "launchpad.tfstate"
-  }
 }
 
 provider "azurerm" {
@@ -32,15 +26,18 @@ locals {
   service_name-int = "terraform"
 }
 
+data "azurerm_resource_group" "rg" {
+  name = "IaC_pipelines_rg"
+}
 
 module "storage_module" {
   source                  = "git::https://github.com/adammontlake/IaC-TF-pipe-demo.git//IaC/modules/storage"
   service_name            = "terraform"
-  resource_group_name     = module.prod_rg_name.full_name
-  location                = local.location
+  resource_group_name     = data.azurerm_resource_group.rg.name
+  location                = data.azurerm_resource_group.rg.location
   account_tier            = "Standard"
   environment             = local.environment
-  terraform_state_storage = true
+  terraform_state_storage = false
   tags = {
     environment = "production"
     costcenter  = "it"
